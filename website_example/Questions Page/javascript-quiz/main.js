4eb00bb617cb5e6b761d9663995d3d4bacfb47ba
robertkister <98337854+robertkister@users.noreply.github.com>
2022-03-23 22:05:48 +0000

    Add files via upload

--- website_example/Questions Page/javascript-quiz/main.js
+++ website_example/Questions Page/javascript-quiz/main.js
@@ -1,124 +1,136 @@
 
-function check(){
 
-	var question1 = document.quiz.question1.value;
-	var question2 = document.quiz.question2.value;
-	var question3 = document.quiz.question3.value;
-	var question4 = document.quiz.question4.value;
-	var question5 = document.quiz.question5.value;
-	var question6 = document.quiz.question6.value;
-	var question7 = document.quiz.question7.value;
-	var question8 = document.quiz.question8.value;
-	var question9 = document.quiz.question9.value;
-	var question10 = document.quiz.question10.value;
-	var question11 = document.quiz.question11.value;
-	var question12 = document.quiz.question12.value;
-	var question13 = document.quiz.question13.value;
-	var question14 = document.quiz.question14.value;
-	var question15 = document.quiz.question15.value;
-	var question16 = document.quiz.question16.value;
-	var question17 = document.quiz.question17.value;
-	var question18 = document.quiz.question18.value;
-	var question19 = document.quiz.question19.value;
-	var question20 = document.quiz.question20.value;
-	var question21 = document.quiz.question21.value;
-	var question22 = document.quiz.question22.value;
 
-	var correct = 0;
+
+function examine(){
+
+
+
+	var question_0 = document.quiz.question_0.value;
+	var question_1 = document.quiz.question_1.value;
+	var question_2 = document.quiz.question_2.value;
+	var question_3 = document.quiz.question_3.value;
+	var question_4 = document.quiz.question_4.value;
+	var question_5 = document.quiz.question_5.value;
+	var question_6 = document.quiz.question_6.value;
+	var question_7 = document.quiz.question_7.value;
+	var question_8 = document.quiz.question_8.value;
+	var question_9 = document.quiz.question_9.value;
+	var question_10 = document.quiz.question_10.value;
+	var question_11 = document.quiz.question_11.value;
+	var question_12 = document.quiz.question_12.value;
+	var question_13 = document.quiz.question_13.value;
+	var question_14 = document.quiz.question_14.value;
+	var question_15 = document.quiz.question_15.value;
+	var question_16 = document.quiz.question_16.value;
+	var question_17 = document.quiz.question_17.value;
+	var question_18 = document.quiz.question_18.value;
+	var question_19 = document.quiz.question_19.value;
+	var question_20 = document.quiz.question_20.value;
+	var question_21 = document.quiz.question_21.value;
+	var question_22 = document.quiz.question_22.value;
+
+	var points = 0;
 
 	
 
 
-	if (question1 == "34282") {
-		correct++;
+	if (question_1 == "34282") {
+		points++;
 }
-	if (question2 == "5250") {
-		correct++;
+	if (question_2 == "5250") {
+		points++;
 }	
-	if (question3 == "April 21,2014") {
-		correct++;
+	if (question_3 == "April 21,2014") {
+		points++;
 }
-	if (question4 == "April 20,2014") {
-		correct ++;
+	if (question_4 == "April 20,2014") {
+		points ++;
 }
-	if (question5 == "5737") {
-		correct ++;
+	if (question_5 == "5737") {
+		points ++;
 }
-	if (question6 == "April 21,2014") {
-		correct ++; 
+	if (question_6 == "April 21,2014") {
+		points ++; 
 }
-	if (question7 == "5224") {
-		correct ++;
+	if (question_7 == "5224") {
+		points ++;
 }
-	if (question8 == "513") {
-		correct ++;
+	if (question_8 == "513") {
+		points ++;
 }
-	if (question9 == "www1/access.log") {
-		correct ++;
+	if (question_9 == "www1/access.log") {
+		points ++;
 }
-	if (question10 == "www1/access.log") {
-		correct ++;
+	if (question_10 == "www1/access.log") {
+		points ++;
 }
-	if (question11 == "128.241.220.82") {
-		correct ++;
+	if (question_11 == "128.241.220.82") {
+		points ++;
 }	
-	if (question12 == "29") {
-		correct ++;
+	if (question_12 == "29") {
+		points ++;
 }
-	if (question13 == "April 24,2014") {
-		correct ++; 
+	if (question_13 == "April 24,2014") {
+		points ++; 
 }
-	if (question14 == "7669") {
-		correct ++; 
+	if (question_14 == "7669") {
+		points ++; 
 }
-	if (question15 == "April 24,2014") {
-		correct ++;
+	if (question_15 == "April 24,2014") {
+		points ++;
 }
-	if (question16 == "401") {
-		correct ++;
+	if (question_16 == "401") {
+		points ++;
 }
-	if (question17 == "1445") {
-		correct ++;
+	if (question_17 == "1445") {
+		points ++;
 }		
-	if (question18 == "276") {
-		correct ++;
+	if (question_18 == "276") {
+		points ++;
 }		
-	if (question19 == "April 19,2014") {
-		correct ++;
+	if (question_19 == "April 19,2014") {
+		points ++;
 }		
-	if (question20 == "33253") {
-		correct ++;
+	if (question_20 == "33253") {
+		points ++;
 }		
-	if (question21 == "98") {
-		correct ++;
+	if (question_21 == "98") {
+		points ++;
 }		
-	if (question22 == "118.142.68.222") {
-		correct ++;
+	if (question_22 == "118.142.68.222") {
+		points ++;
+}
 	
-	
-}	
-	
-	var pictures = ["img/win.gif", "img/meh.jpeg", "img/lose.gif"];
-	var messages = ["Great job!", "That's just okay", "You really need to do better"];
-	var score;
 
-	if (correct <= 8 ) {
-		score = 2;
+
+
+	
+	
+	var images = ["img/maximum.gif", "img/average.gif", "img/minimum.gif"];
+	var notes = ["Look at those skills!", "That's not bad", "You can do better"];
+	
+	var result;
+
+	if (points <= 8 ) {
+		result = 2;
 	}
 
-	if (correct > 8 && correct <= 16) {
-		score = 1;
+	if (points > 8 && points <= 16) {
+		result = 1;
 	}
 
-	if (correct > 16 ) {
-		score = 0;
+	if (points > 16 ) {
+		result = 0;
 	}
 
 	
 
-	document.getElementById("after_submit").style.visibility = "visible";
+	document.getElementById("submission").style.visibility = "visible";
 
-	document.getElementById("message").innerHTML = messages[score];
-	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
-	document.getElementById("picture").src = pictures[score];
+	document.getElementById("note").innerHTML = notes[result];
+	document.getElementById("number_points").innerHTML =   "You have earned " + points + " point/s.";
+	document.getElementById("name").innerHTML = question_0 ;
+
+	document.getElementById("picture").src = images[result];
 	}
